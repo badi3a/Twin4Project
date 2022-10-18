8064dc59ea2cb68aaef242f2aa7fe55d4968da80
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-10-18 11:59:53 +0200

    session 6.2

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,16 +1,16 @@
 import { FormProductComponent } from './form-product/form-product.component';
 import { ListProductComponent } from './list-product/list-product.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
 
 const routes: Routes = [
-  { path: '', component: ProductsComponent,children:[
-      {path:'', component: ListProductComponent},
-      {path:'list', component: ListProductComponent},
-      {path:'new', component: FormProductComponent}
-  ] }
-];
+  {path:'',component: ProductsComponent,children:[
+  {path:'', component: ListProductComponent},
+  {path:'list',component: ListProductComponent},
+  {path:'new', component: FormProductComponent},
+  {path:'category/:category', component:ListProductComponent}]}]
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
